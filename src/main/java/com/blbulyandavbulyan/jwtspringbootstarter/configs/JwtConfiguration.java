171d94133d5603300f6d86d38a498c106711e832
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-04 12:24:33 +0400

    now bean JwtConfigurationProperties is also defined in JwtConfiguration

--- src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfiguration.java
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfiguration.java
@@ -2,6 +2,7 @@ package com.blbulyandavbulyan.jwtspringbootstarter.configs;
 
 import com.blbulyandavbulyan.jwtspringbootstarter.filters.JwtRequestFilter;
 import com.blbulyandavbulyan.jwtspringbootstarter.services.TokenService;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,5 +15,10 @@ public class JwtConfiguration {
     @Bean
     public JwtRequestFilter jwtRequestFilter(TokenService tokenService){
         return new JwtRequestFilter(tokenService);
+    }
+    @Bean
+    @ConfigurationProperties(prefix = "jwt-spring-boot-starter")
+    public JwtConfigurationProperties jwtConfigurationProperties(){
+        return new JwtConfigurationProperties();
     }
 }
--- src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfigurationProperties.java
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfigurationProperties.java
@@ -1,12 +1,6 @@
 package com.blbulyandavbulyan.jwtspringbootstarter.configs;
-
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Component;
-
 import java.time.Duration;
 
-@Component
-@ConfigurationProperties(prefix = "jwt-spring-boot-starter")
 public class JwtConfigurationProperties {
     private Duration lifetime;
 
