5fa954390e1b5cc207a888582c5f9cf450f1f380
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-04 11:12:43 +0400

    initial commit

---
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/JwtSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.jwtspringbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtSpringBootStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/blbulyandavbulyan/jwtspringbootstarter/JwtSpringBootStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blbulyandavbulyan.jwtspringbootstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSpringBootStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
