7780f53b1c722bb0084a899cd36fac84eabaefa4
David Blbulyan <blbulyandavbulyan@gmail.com>
2023-12-11 18:05:16 +0400

    added signatureAlgorithm in JwtConfigurationProperties, also added @Getter, @Setter annotations.

--- src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfigurationProperties.java
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfigurationProperties.java
@@ -1,14 +1,13 @@
 package com.blbulyandavbulyan.jwtspringbootstarter.configs;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.Duration;
 
+@Setter
+@Getter
 public class JwtConfigurationProperties {
-    private Duration lifetime;
-
-    public Duration getLifetime() {
-        return lifetime;
-    }
-
-    public void setLifetime(Duration lifetime) {
-        this.lifetime = lifetime;
-    }
+    private Duration lifetime = Duration.ofMinutes(10);
+    private SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
 }
