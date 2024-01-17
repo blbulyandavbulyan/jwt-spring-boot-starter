61b9b85ce011c4bdcb1703a8131c6fe73fb3b2b5
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-17 22:26:22 +0400

    removed all manual beans creation from JwtConfiguration

--- src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfiguration.java
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/configs/JwtConfiguration.java
@@ -1,24 +1,9 @@
 package com.blbulyandavbulyan.jwtspringbootstarter.configs;
 
-import com.blbulyandavbulyan.jwtspringbootstarter.filters.JwtRequestFilter;
-import com.blbulyandavbulyan.jwtspringbootstarter.services.TokenService;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@ComponentScan(basePackages = "com.blbulyandavbulyan.jwtspringbootstarter")
 public class JwtConfiguration {
-    @Bean
-    public TokenService tokenService(JwtConfigurationProperties jwtConfigurationProperties){
-        return new TokenService(jwtConfigurationProperties);
-    }
-    @Bean
-    public JwtRequestFilter jwtRequestFilter(TokenService tokenService){
-        return new JwtRequestFilter(tokenService);
-    }
-    @Bean
-    @ConfigurationProperties(prefix = "jwt-spring-boot-starter")
-    public JwtConfigurationProperties jwtConfigurationProperties(){
-        return new JwtConfigurationProperties();
-    }
 }
