cef1de74bd32f22ce3dd633d1e982dd01ea26b09
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-17 22:52:17 +0400

    fixed JwtSpringBootStarterApplicationTests

--- src/test/java/com/blbulyandavbulyan/jwtspringbootstarter/JwtSpringBootStarterApplicationTests.java
+++ src/test/java/com/blbulyandavbulyan/jwtspringbootstarter/JwtSpringBootStarterApplicationTests.java
@@ -1,9 +1,14 @@
 package com.blbulyandavbulyan.jwtspringbootstarter;
 
+import com.blbulyandavbulyan.jwtspringbootstarter.configs.JwtConfiguration;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
-@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = JwtConfiguration.class)
 class JwtSpringBootStarterApplicationTests {
 
     @Test
