5367c9a9055be47a7e2b421794ce3cfe4eccbe9d
David Blbulyan <blbulyandavbulyan@gmail.com>
2024-01-17 22:25:25 +0400

    added @Component annotation for JwtRequestFilter

--- src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/filters/JwtRequestFilter.java
+++ src/main/java/com/blbulyandavbulyan/jwtspringbootstarter/filters/JwtRequestFilter.java
@@ -12,6 +12,7 @@ import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -19,6 +20,7 @@ import java.io.IOException;
 /**
  * Данный фильтр управляет jwt аутентификацией
  */
+@Component
 @RequiredArgsConstructor
 public class JwtRequestFilter extends OncePerRequestFilter {
     private final TokenService tokenService;
